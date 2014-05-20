d267d897dc3b7b8b449d200d3e6c8eb89d4c88ba
Jeremagician <jeremagician@gmail.com>
2014-05-20 12:48:07 +0200

    Revert "junked out presentation"
    This reverts commit 182024cacf3c324f9e1158b3c3860cbbe2657fbd.

--- src/game.hpp
+++ src/game.hpp
@@ -13,10 +13,18 @@ namespace tafl
 {
 	class game
 	{
+		friend class network::network_manager;
 	public:
 		game(int argc, char** argv);
 		virtual ~game(void);
 		int operator()(void);
+
+		enum team
+		{
+			MUSCOVITE = 0,
+			SWEDISH
+		};
+
 	private:
 		void update(uint32_t diff);
 		void render(void);
@@ -38,6 +46,9 @@ namespace tafl
 		camera *camera_;
 		unsigned max_framerate_;
 		cell selected_;
+		game::team team_;
+		bool is_team(board::pawn_type type);
+		bool should_play_;
 		network::network_manager net{this};
 	};
 }
